94b884d06439fed75d855910a981a14a06399a84
Henry Knox <hdkno577@learner.dvusd.org>
2023-02-22 09:30:46 -0700

    Camera on bus but is wrong

---
+++ Road Test/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject bus;
+    private Vector3 offset = new Vector3(-0.75f, 2, 5);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = bus.transform.position + offset;
+        transform.rotation = bus.transform.rotation;
+    }
+}
