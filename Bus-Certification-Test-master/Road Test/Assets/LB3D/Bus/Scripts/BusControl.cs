0664563d3fc060296c241b7ab3256a7ded8ff6dc
Ayden <armac417@learner.dvusd.org>
2023-03-02 08:33:46 -0700

    3/2/we back

---
+++ Bus-Certification-Test-master/Road Test/Assets/LB3D/Bus/Scripts/BusControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusControl : MonoBehaviour {
+
+    /// <summary>
+    /// This is an example script for controlling the doors and signs on the bus. 
+    /// You may wish to create a more specific implementation consistent with your game.
+    /// </summary>
+
+    public Animator Door1;
+    public Animator Door2;
+    public Animator StopSign1;
+    public Animator StopSign3;
+    private float speed = 40.0f;
+    private float turnspeed = 80.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    public float OpenCloseSpeed;
+
+    private bool isOpen = false;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+
+        if (forwardInput == 0.0f)
+            turnspeed = 0;
+        else if (forwardInput > 0.0f)
+            turnspeed = 45.0f;
+        if (forwardInput == 0.0f)
+            turnspeed = 0;
+        else if (forwardInput < 0.0f)
+            turnspeed = 45.0f;
+
+
+
+    }
+
+    public void Open(bool open = true) {
+        string action = "";
+        if (open)
+        {
+            action = "Open";
+        }
+        else {
+            action = "Close";
+        }
+        Door1.SetTrigger(action);
+        Door2.SetTrigger(action);
+        StopSign1.SetTrigger(action);  
+        StopSign3.SetTrigger(action);
+        isOpen = !isOpen;
+    }
+
+    public void ToggleDoor() {
+        if (isOpen)
+        {
+            Open(false);
+        }
+        else {
+            Open(true);
+        }
+    }
+
+}
